b682bbd929756593571218a871087a31701e3790
antoniolig <giorgio.antonioli.dev@gmail.com>
2019-07-14 18:57:28 +0200

    Add label view

--- IOSNativeBasedSample/ViewController.swift
+++ IOSNativeBasedSample/ViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
-
-
 }
-
