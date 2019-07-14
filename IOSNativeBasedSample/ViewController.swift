43af592044f30430a9020d99110e26323f2f9c67
antoniolig <giorgio.antonioli.dev@gmail.com>
2019-07-14 18:28:51 +0200

    Initial Commit

---
+++ IOSNativeBasedSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSNativeBasedSample
+//
+//  Created by Giorgio Antonioli on 14/07/2019.
+//  Copyright © 2019 Fondesa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
